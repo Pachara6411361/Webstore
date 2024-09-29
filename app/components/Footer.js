92fbbe884f3875c89fa9875f3a04f060c90a04ae
Phutthiphat <90144841+phutthiphathut@users.noreply.github.com>
2024-09-29 20:03:41 +0700

    update app name

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Gaming Gear Store. All Rights Reserved.</p>
+      <p>&copy; 2024 Deejai Gaming Gear. All Rights Reserved.</p>
       <ul className="footer-links">
         <li>
           <a href="/">Privacy Policy</a>
